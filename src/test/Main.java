bb981e001a5b2694e58b7a89db1c15ec57a29a91
ABlued <skwodhkswkd@naver.com>
2020-10-28 01:05:39 +0900

    4673번 셀프 넘버

---
+++ src/test/Main.java
@@ -0,0 +1,29 @@
+package test;
+
+public class Main {
+	public static void main(String[] args) {
+		boolean[] resultMap = new boolean[10001];
+
+		for(int i = 1; i < 10000; i++) {
+			int n = d(i);
+			if(n <= 10000) {
+				resultMap[n] = true;				
+			}
+		}
+		for(int i = 1; i < 10000; i++) {
+			if(resultMap[i] == false ) {
+				System.out.println(i);
+			}
+		}
+	}
+
+	static int d(int n) {
+		int sum = n;
+		while(n != 0){
+			sum = sum + (n % 10);// 자릿수를 더해준다
+			n = n/10;	//자릿수를 한번 낮춘다
+		}
+		return sum;
+	}
+}
+
